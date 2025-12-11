006ce8ed7dd43cf47efab481d670fc222e6373f2
Akadirr1 <akadirr41@outlook.com>
2025-12-11 17:39:06 +0300

    auth and adminValidation middlewares added

---
+++ MiddleWares/AdminMiddleWare.js
@@ -0,0 +1,23 @@
+
+const AdminMW = async (ctx, next) => {
+	try {
+		const user = ctx.state.user
+		if (!user) {
+			ctx.status = 401
+			ctx.body = { message: "Kullanıcı bulunamadı " }
+			return
+		}
+		if (user.isAdmin == false) {
+			ctx.status = 401
+			ctx.body = { message: "Admin Yetkisi yok" }
+			return
+		}
+		await next()
+	} catch (error) {
+		ctx.status = 500
+		ctx.body = { message: "Sunucuda admin doğrulama hatası" }
+		console.log(error)
+	}
+}
+
+module.exports = { AdminMW }
